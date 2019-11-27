9757e117e009274a3f81b98b063ece909fc19269
Mahima Agrawal <mahi9.goyal@gmail.com>
2019-11-27 09:40:57 +0530

    Added comment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const customers = require('./Customer.json');
 
+/**
+ * @param object, Latitude and longitude of customer
+ * @param object Latitude and longitude of Destination point(in this case Dublin)
+ * @returns distance between the specified points in number
+ */
 function distance(checkPoint, centerPoint) {
     var ky = 40000 / 360;
     var kx = Math.cos(Math.PI * centerPoint.latitude / 180.0) * ky;
@@ -8,6 +13,7 @@ function distance(checkPoint, centerPoint) {
     return Math.sqrt(dx * dx + dy * dy);
 }
 
+//This function will calculate the distance between customer and dublin one by one and will return customer's name and UserID
 function findCustomersInRange(destinationPoints, desiredDistanceInKm) {
     const matchedCustomer = customers.map(customer => {
         const { name, user_id: userID } = customer;
@@ -20,5 +26,7 @@ function findCustomersInRange(destinationPoints, desiredDistanceInKm) {
     }).filter(item => item!== undefined).sort((a, b) => a.userID - b.userID);
     return matchedCustomer
 }
-const customerInDesiredDistance = findCustomersInRange({ latitude: 53.339428, longitude: -6.257664 }, 100);
+
+const dublinPoints = { latitude: 53.339428, longitude: -6.257664 };
+const customerInDesiredDistance = findCustomersInRange(dublinPoints, 100);
 console.log('customer to send order in 100 km range from Dublin', customerInDesiredDistance);
